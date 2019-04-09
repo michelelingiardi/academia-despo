c2016f969cbc5c92629ca5cb6a27e241a6e1aa7e
35291787805 <michele.lingiardi@serpro.gov.br>
2019-04-09 18:10:36 -0300

    Versão inicial.

---
+++ CamelCase/src/main/java/despo/academia/CamelCase/App.java
@@ -0,0 +1,13 @@
+package despo.academia.CamelCase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

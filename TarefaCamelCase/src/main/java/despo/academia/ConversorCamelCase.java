e8bf7cda595711369c7e7456c162b90d74ccb9c7
Michele <michelelingiardi@gmail.com>
2019-04-13 11:46:00 -0300

    Código de produção.

--- TarefaCamelCase/src/main/java/despo/academia/ConversorCamelCase.java
+++ TarefaCamelCase/src/main/java/despo/academia/ConversorCamelCase.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConversorCamelCase {
-	public static List<String> converterCamelCase(String original) {
-	  List<String> lista = new ArrayList<String>();
-	  lista.add(original.toLowerCase());
-		return lista;
+  public static List<String> converterCamelCase(String original) {
+    List<String> lista = new ArrayList<String>();
+    String[] separated = original.split("(?=[A-Z])");
+    for (String palavra : separated) {
+      lista.add(palavra.toLowerCase());
+    }    
+    return lista;
 	}
 }

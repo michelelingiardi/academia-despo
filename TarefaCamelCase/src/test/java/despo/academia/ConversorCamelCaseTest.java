f533da5cb8028d1937771672819a2ef0d0d8dc4b
Michele <michelelingiardi@gmail.com>
2019-04-12 22:18:48 -0300

    Teste "Nome".

--- TarefaCamelCase/src/test/java/despo/academia/ConversorCamelCaseTest.java
+++ TarefaCamelCase/src/test/java/despo/academia/ConversorCamelCaseTest.java
@@ -8,11 +8,18 @@ import org.junit.Test;
 import despo.academia.ConversorCamelCase;
 import junit.framework.TestCase;
 
-public class CamelCaseTest extends TestCase{
+public class ConversorCamelCaseTest extends TestCase{
 	@Test
 	public void testCamelCaseSimples() {
 		assertEquals(
 				new ArrayList<String>(Arrays.asList("nome")),
 				ConversorCamelCase.converterCamelCase("nome"));
+	}
+	
+	@Test
+	public void testCamelCaseLetraMaiuscula() {
+	  assertEquals(
+	      new ArrayList<String>(Arrays.asList("nome")),
+	      ConversorCamelCase.converterCamelCase("Nome"));
 	}
 }

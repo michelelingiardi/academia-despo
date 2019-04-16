0f8c25f7433863533fcf2e79a8b90228bc341b27
Michele <michelelingiardi@gmail.com>
2019-04-15 21:13:57 -0300

    Exercício Extra TDD: Abastecer com etanol ou gasolina? - Versão inicial.

---
+++ ComparadorCombustivel/src/main/java/foo/IntegracaoConsultaCombustivelIndisponivelException.java
@@ -0,0 +1,11 @@
+package foo;
+
+public class IntegracaoConsultaCombustivelIndisponivelException extends RuntimeException {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  public IntegracaoConsultaCombustivelIndisponivelException() {
+    super("Consulta indisponível.");
+  }
+}
---
+++ ComparadorCombustivel/src/main/java/foo/PrecoCombustivel.java
@@ -0,0 +1,15 @@
+package foo;
+import java.util.Random;
+
+public class PrecoCombustivel {
+  public double obterPrecoCombustivel(String siglaCombustivel) {
+    Random random = new Random();
+    double minValue = 1;
+    double maxValue = 10;
+    boolean ocorreuErro = (random.nextInt(100) == 1);
+    if (ocorreuErro) {
+       throw new IntegracaoConsultaCombustivelIndisponivelException();
+    }
+    return random.nextDouble() * (maxValue - minValue) + minValue;
+  } 
+}

b97b753f701b2d6e2d6e4d2f698bd6ba2e22febd
Michele <michelelingiardi@gmail.com>
2019-04-15 22:27:09 -0300

    Código de produção.

--- ComparadorCombustivel/src/main/java/despo/academia/ComparadorDePrecos.java
+++ ComparadorCombustivel/src/main/java/despo/academia/ComparadorDePrecos.java
@@ -5,6 +5,8 @@ import foo.PrecoCombustivelWS;
 public class ComparadorDePrecos {
   private static final String ETANOL = "E";
   private static final String GASOLINA = "G";
+  public static final String MSG_ETANOL = "Compre etanol.";
+  public static final String MSG_GASOLINA = "Compre gasolina.";
   private PrecoCombustivelWS precoCombustivel;
 
   public ComparadorDePrecos(PrecoCombustivelWS precoCombustivel) {
@@ -17,5 +19,10 @@ public class ComparadorDePrecos {
 
   public double obterPrecoGasolina() {
     return this.precoCombustivel.obterPrecoCombustivel(GASOLINA);
+  }
+
+  public String comparar() {
+    double valor = (this.obterPrecoEtanol() * 100 / this.obterPrecoGasolina()) - 100 * -1 ;
+    return (valor <= 70) ? MSG_ETANOL : MSG_GASOLINA;
   }
 }

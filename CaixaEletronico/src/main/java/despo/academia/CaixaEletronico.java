4f779ac24358ef3bca25fe88e0a875b4fcb387ce
aluno <aluno@10.32.16.20>
2019-05-15 15:23:27 -0300

    código de produção

--- CaixaEletronico/src/main/java/despo/academia/CaixaEletronico.java
+++ CaixaEletronico/src/main/java/despo/academia/CaixaEletronico.java
@@ -1,8 +1,19 @@
 package despo.academia;
+
 public class CaixaEletronico {
+	private Hardware hardware;
+
+	public CaixaEletronico(Hardware hardware) {
+		this.hardware = hardware;
+	}
 
 	public String logar() {
-		return "Usuário Autenticado";
+		try {
+			hardware.pegarNumeroDaContaCartao();
+			return "Usuário Autenticado";
+		} catch(Exception e) {
+			return "Não foi possível autenticar o usuário";
+		}
 	}
 
 }

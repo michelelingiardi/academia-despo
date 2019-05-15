17e2b181590c0ca507e912a8ffde4367d0749609
aluno <aluno@10.32.16.20>
2019-05-15 15:13:13 -0300

    teste

--- CaixaEletronico/src/test/java/CaixaEletronicoTest.java
+++ CaixaEletronico/src/test/java/CaixaEletronicoTest.java
@@ -1,15 +1,31 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import despo.academia.CaixaEletronico;
 
 public class CaixaEletronicoTest {
-
+	HardwareMock hardwareMock;
+	
+	@Before
+	public void before() {
+		hardwareMock = new HardwareMock();
+	}
+	
+	
 	@Test
 	public void logarComSucesso() {
-		CaixaEletronico c = new CaixaEletronico();
+		CaixaEletronico c = new CaixaEletronico(hardwareMock);
 		String mensagemRecebida = c.logar();
 		assertEquals("Usuário Autenticado", mensagemRecebida);
+	}
+
+	@Test
+	public void logarComErro() {
+		hardwareMock.testarUsuarioInvalido();
+		CaixaEletronico c = new CaixaEletronico(hardwareMock);
+		String mensagemRecebida = c.logar();		
+		assertEquals("Não foi possível autenticar o usuário", mensagemRecebida);
 	}
 }

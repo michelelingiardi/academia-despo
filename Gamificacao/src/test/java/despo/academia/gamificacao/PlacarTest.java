e1d56a6bb7055b2be25f3414322f81888e9d1907
Michele <michele.lingiardi@serpro.gov.br>
2019-06-10 15:44:35 -0300

    teste

--- Gamificacao/src/test/java/despo/academia/gamificacao/PlacarTest.java
+++ Gamificacao/src/test/java/despo/academia/gamificacao/PlacarTest.java
@@ -8,11 +8,21 @@ import org.junit.jupiter.api.Test;
 public class PlacarTest {
 
 	@Test
-	public void registrarPontoParaUsuario() {
+	public void registrarPontosParaUsuario() {
 		ArmazenamentoMock mock = new ArmazenamentoMock();
 		Placar placar = new Placar(mock);
 		placar.registrarPontoParaUsuario("estrela", "guerra", 10);
 		assertTrue(mock.chamouArmazenarUsuario);
 		assertEquals(10, mock.getPontuacaoUsuario("estrela", "guerra"));
+	}
+	
+	@Test
+	public void registrarMaisPontosParaUsuario() {
+		ArmazenamentoMock mock = new ArmazenamentoMock();
+		Placar placar = new Placar(mock);
+		placar.registrarPontoParaUsuario("estrela", "guerra", 10);		
+		assertEquals(10, mock.getPontuacaoUsuario("estrela", "guerra"));
+		placar.registrarPontoParaUsuario("estrela", "guerra", 5);
+		assertEquals(15, mock.getPontuacaoUsuario("estrela", "guerra"));
 	}
 }
